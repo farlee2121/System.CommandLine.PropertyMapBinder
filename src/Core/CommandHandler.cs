b8d849de3377bc85457742443f5b60353c8e46c9
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-03-04 17:14:09 -0600

    Make command PropertyBinder -> CommandHandler functions internal since pipeline is the supported route for now

--- src/Core/CommandHandler.cs
+++ src/Core/CommandHandler.cs
@@ -11,9 +11,8 @@ using System.Threading.Tasks;
 namespace System.CommandLine.PropertyMapBinder
 {
 
-    public class CommandHandler
+    internal class CommandHandler
     {
-        // IDEA: TODO: assuming a default constructor is a bit dangerous. I should probably allow some way to provide the base config instance or a factory (context) => base instance 
         public static ICommandHandler FromPropertyMap<InputModel>(Action<InputModel> handler, IPropertyBinder<InputModel> propertyBinder) where InputModel : new()
         {
             
