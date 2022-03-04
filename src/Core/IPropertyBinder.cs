388f7c27edae43fdb276044684575dbcb393afa6
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-03-04 17:10:31 -0600

    Fix capitalization in binder interface

--- src/Core/IPropertyBinder.cs
+++ src/Core/IPropertyBinder.cs
@@ -5,6 +5,6 @@ namespace System.CommandLine.PropertyMapBinder
 {
     public interface IPropertyBinder<InputModel>
     {
-        InputModel Bind(InputModel InputModel, InvocationContext context);
+        InputModel Bind(InputModel inputModel, InvocationContext context);
     }
 }
