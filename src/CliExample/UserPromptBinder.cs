5c23e3c50675c41bd1d2ea45f35e21b181eb00e9
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-03-04 21:18:44 -0600

    Update NameConventionBinder to depend on latest package version

--- src/CliExample/UserPromptBinder.cs
+++ src/CliExample/UserPromptBinder.cs
@@ -34,7 +34,7 @@ namespace System.CommandLine.PropertyMapBinder.CliExample
 
         public static BinderPipeline<InputModel> PromptIfMissing<InputModel, T>(this BinderPipeline<InputModel> pipeline, string prompt, Expression<Func<InputModel, T>> selectorLambda)
         {
-            var binder =PropertyBinder.FromFunc((InputModel model, InvocationContext context) =>
+            var binder = FuncPropertyBinder.FromFunc((InputModel model, InvocationContext context) =>
             {
                 var modelValue = selectorLambda.Compile().Invoke(model);
                 if (modelValue == null)
@@ -87,6 +87,30 @@ namespace System.CommandLine.PropertyMapBinder.CliExample
             return pipeline;
         }
 
+    }
+
+    internal class FuncPropertyBinder<InputModel> : IPropertyBinder<InputModel>
+    {
+        private readonly Func<InputModel, InvocationContext, InputModel> setter;
+
+        public FuncPropertyBinder(Func<InputModel, InvocationContext, InputModel> setter)
+        {
+            this.setter = setter;
+        }
+        public InputModel Bind(InputModel InputModel, InvocationContext context)
+        {
+            return setter(InputModel, context);
+        }
+    }
+
+    internal static class FuncPropertyBinder
+    {
+        //TODO: Consider how these constructors should be made available. Putting them all in PropertyBinder like it's a module 
+        // may not be intuitive for C# users
+        public static FuncPropertyBinder<InputModel> FromFunc<InputModel>(Func<InputModel, InvocationContext, InputModel> setter)
+        {
+            return new FuncPropertyBinder<InputModel>(setter);
+        }
     }
 
     internal static class ReflectionHelper
