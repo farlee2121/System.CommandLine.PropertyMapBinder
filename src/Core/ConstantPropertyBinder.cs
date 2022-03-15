e4c458291eaaf6fcc038ecd7bb05a73efae30045
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2022-03-15 16:28:35 -0500

    Create a constant property binder

---
+++ src/Core/ConstantPropertyBinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine.Invocation;
+using System.Text;
+
+namespace System.CommandLine.PropertyMapBinder
+{
+    internal class ConstantPropertyBinder<TInputModel, TProperty> : IPropertyBinder<TInputModel>
+    {
+        private readonly TProperty value;
+        private readonly Func<TInputModel, TProperty, TInputModel> _propertySetter;
+
+        public ConstantPropertyBinder(TProperty value, Func<TInputModel, TProperty, TInputModel> setter)
+        {
+            this.value = value;
+            _propertySetter = setter;
+        }
+        public TInputModel Bind(TInputModel inputModel, InvocationContext context)
+        {
+            return _propertySetter(inputModel, value);
+        }
+    }
+}
